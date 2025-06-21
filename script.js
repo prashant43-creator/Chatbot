f0c37bd7acd0d03dc249b6ae935795ec393bf678
Prashant Shinde <prashanshindr853@gmail.com>
2025-06-21 13:43:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const userInput = document.getElementById("user-input");
+const btn = document.getElementById("send-btn");
+const chatbox = document.getElementById("chat-box");
+
+btn.addEventListener("click",()=> {
+  
+  const input = userInput.value.trim();
+  if (input !== "") {
+    addmessage("user",input);
+    botreply(input)
+    userInput.value = "";
+  }
+}  )
+function addmessage(sender,text){
+  const message = document.createElement("div");
+  message.classList.add("message",sender)
+  message.innerText = text ;
+  chatbox.appendChild(message)
+   chatbox.scrollTop = chatbox.scrollHeight ;
+  
+}
+function botreply(input){
+  let response = ""; 
+   const lowercase = input.toLowerCase()
+   if (lowercase.includes("hi")||lowercase.includes("hello")  ) {
+     response = "yes sir , how are you ?"
+   }else if (lowercase.includes("fine") ) {
+     response = `ok.
+     
+     sir which Programming langauge you are learning now 🤔??
+     `
+   }else if(lowercase.includes("javascript")  ){
+     response = "nice sir. keep learning.🤗"
+   }else if(lowercase.includes("thank you")  ){
+     response = "welcome!"
+   }else if(lowercase.includes("bye")  ){
+     response = "Goodbye! Have a nice day"
+   }
+   
+   else{
+     response = "Sorry I didn't understand that."
+   }
+   
+   
+   
+   
+   setTimeout(()=> {
+     addmessage("bot",response )
+     
+   },500)
+}
